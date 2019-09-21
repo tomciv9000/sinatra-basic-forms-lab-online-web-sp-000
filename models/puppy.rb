2b0c83bc31f12d67ee442456d5d47467269e2c7b
Tom White <eugene.white@gmail.com>
2019-09-21 05:28:10 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,8 +2,10 @@ class Puppy
   
   attr_accessor :name, :breed, :months_old
   
-  def initialize
-    
+  def initialize(name, breed, months_old)
+    @name = name
+    @breed = breed
+    @months_old = months_old
   end
   
 end
